09053ff58dd808bbd1e0971b82f22ebadf916686
Courie <laurisjump@gmail.com>
2022-10-01 14:32:27 +0300

    fixed some things x2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+#I've included this as our beloved Kestutis is on linux
 #!/usr/bin/env python3
 
 # the library that we will use for the project
 import tcod
 
 # importing player movement
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 def main() -> None:
+    # this is the size of the terminal
     screen_width = 80
     screen_height = 50
+    # this is the size of our game map
+    map_width = 80
+    map_height = 45
 
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
@@ -24,6 +30,11 @@ def main() -> None:
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
     npc = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -34,24 +45,12 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(player_x, player_y, string="@")
-            context.present(root_console)
+                engine.render(console=root_console, context=context)
 
-            root_console.clear()
+                events = tcod.event.wait()
 
-            for event in tcod.event.wait():
-                
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+                engine.handle_events(events)
+          
 
 if __name__ =="__main__":
     main()
